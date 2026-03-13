65089c3c66545cea859c7079611e915c917cfb89
AdmiralRadish <ultimaxx9@gmail.com>
2026-03-12 19:26:11 -0600

    v1.0.0 with LMP support

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KOSpopupStop")]
+[assembly: AssemblyDescription("Suppresses repeated kOS connectivity manager selection popups by stabilizing persisted manager state")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: ComVisible(false)]
